4b01383b0080aa39c2772a4cf77fd84279f96b98
chaluemwut <chaluemwut@hotmail.com>
2017-06-04 22:32:49 +0700

    start project

--- ui/config.py
+++ ui/config.py
@@ -2,12 +2,13 @@ base_filepath = '/data/cred_frame_data'
 model_model_filepath = base_filepath+'/model'
 
 # API
-api_url = 'http://fbcredibility.com/api/process'
+api_url = 'https://fbcredibility.com/api/process'
 
 # database
 db_host='localhost'
 
+import os
 from pymongo import MongoClient
-# client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
-client = MongoClient(host=db_host)
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
+# client = MongoClient(host=db_host)
 db = client['credframe']
